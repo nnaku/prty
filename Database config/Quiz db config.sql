af15e6fd381a4a37094f834258206ddf856432ce
nnaku <aku.kangas@metropolia.fi>
2017-09-12 05:58:43 +0300

    Games db config

---
+++ Database config/Quiz db config.sql
@@ -0,0 +1,28 @@
+DROP DATABASE Quiz;
+CREATE DATABASE Quiz;
+USE Quiz;
+
+CREATE TABLE Questions (
+    ID int AUTO_INCREMENT UNIQUE NOT NULL,
+	Author varchar(255) NOT NULL,
+    Private BOOLEAN default 1 NOT NULL,
+    Question TEXT NOT NULL,
+    Correct varchar(255) UNIQUE NOT NULL,
+    False1 varchar(255) UNIQUE NOT NULL,
+    False2 varchar(255) UNIQUE NOT NULL,
+    False3 varchar(255) UNIQUE NOT NULL,
+    False4 varchar(255),
+    False5 varchar(255),
+    False6 varchar(255),
+    False7 varchar(255),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Games (
+    ID INT AUTO_INCREMENT UNIQUE NOT NULL,
+    Author VARCHAR(255) NOT NULL,
+    GameName VARCHAR(255) NOT NULL,
+    Private BOOLEAN default 1 NOT NULL,
+    Questions LONGTEXT NOT NULL,
+    PRIMARY KEY (ID)
+);
