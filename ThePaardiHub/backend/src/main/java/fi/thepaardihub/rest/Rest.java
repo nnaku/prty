374ea1a4e46711b6339c7c289ffeedf3b89291e5
Matti <mara.holopainen@gmail.com>
2017-09-23 18:29:10 +0300

    Added rest that adds fake data

--- ThePaardiHub/backend/src/main/java/fi/thepaardihub/rest/Rest.java
+++ ThePaardiHub/backend/src/main/java/fi/thepaardihub/rest/Rest.java
@@ -65,5 +65,29 @@ public class Rest {
 		return users.allRoles();
 
 	}
+	@RequestMapping("/fake")
+	public void fakeData() {
+
+		
+		UserRoles banned = new UserRoles(0,"Banned");
+		UserRoles member = new UserRoles(1,"Member");
+		UserRoles admin = new UserRoles(9,"Admin");
+		UserRoles god = new UserRoles(99,"God");
+		
+		users.saveOrUpdateRoles(banned);
+		users.saveOrUpdateRoles(member);
+		users.saveOrUpdateRoles(admin);
+		users.saveOrUpdateRoles(god);
+		
+		UserAccounts add1 = new UserAccounts("nAku","asfeqkfjhseaoiutfrt","Aku","Kangas","Aku.Kangas@metropolia.fi");
+		UserAccounts add2 = new UserAccounts("Matti","asfeqkfjhseaoiutfrt","Matti","Holopainen","Matti.holopainen2@metropolia.fi");
+		UserAccounts add3 = new UserAccounts("Tiina","asfeqkfjhseaoiutfrt","Tiina","Ojala","Tiina.Ojala3@metropolia.fi");
+		UserAccounts add4 = new UserAccounts("Maarit","asfeqkfjhseaoiutfrt","Maarit","Saariniemi","Maarit.Saariniemi@Metropolia.fi");
+		
+		users.saveOrUpdateAccount(add1);
+		users.saveOrUpdateAccount(add2);
+		users.saveOrUpdateAccount(add3);
+		users.saveOrUpdateAccount(add4);
+	}
 
 }
