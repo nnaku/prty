66a87fe17af999006800e4c06ade6f7b6b79a046
Aku Kangas <akukan@metropolia.fi>
2017-09-28 14:10:47 +0300

    surefire

--- ThePaardiHub/frontend/src/App.vue
+++ ThePaardiHub/frontend/src/App.vue
@@ -1,15 +1,13 @@
 <style src="./components/Styles/styles.css"></style>
-
 <template>
   <div id="app">
-
     <div class="header">
-        <p>App.vue: Joten navit headerit yms yms. tänne!</p>
+      <p>App.vue: Joten navit headerit yms yms. tänne!</p>
     </div>
     <p>App.vue: Tää näkyy näkyy joka sivulle!</p>
     <router-view></router-view>
     <div class="footer">
-        <p>App.vue: Esim footer route-view:n alle</p>
+      <p>App.vue: Esim footer route-view:n alle</p>
     </div>
   </div>
 </template>
