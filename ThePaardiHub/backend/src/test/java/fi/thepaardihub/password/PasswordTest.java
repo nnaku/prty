2b322e4a90bb74ce3c66bf68cd49dbcd235a8dc1
akukan <akukan@DESKTOP-BSA588S.wlan.metropolia.fi>
2017-10-02 10:17:32 +0300

    Password JUnits

---
+++ ThePaardiHub/backend/src/test/java/fi/thepaardihub/password/PasswordTest.java
@@ -0,0 +1,55 @@
+package fi.thepaardihub.password;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+
+import fi.thepaardihub.password.Password;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class PasswordTest {
+	Password passwd = new Password();
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+	
+	@Test
+	public void basicPasswordFunctions() throws Exception {
+		String validpw = "Val1dtP4ss";
+		String falsepw = "F4ls3P4ss";
+		String validHash  = passwd.getSaltedHash(validpw);
+		String collisionHash = passwd.getSaltedHash(validpw);
+		
+		assertNotNull(validHash);
+		assertNotNull(collisionHash);
+		assertNotEquals(validHash, collisionHash);
+		assertTrue(passwd.validPassword(validpw, validHash));
+		assertFalse(passwd.validPassword(falsepw, validHash));
+		
+	}
+	@Test
+	public void invalidSaltHash() throws Exception {
+		exception.expect(IllegalStateException.class);
+		passwd.validPassword("Password123", "thisIsHashWithoutSalt");
+	}
+	
+	@Test
+	public void emptyPasswors() throws Exception {
+		exception.expect(IllegalArgumentException.class);
+		passwd.getSaltedHash("");
+	}
+	
+	@Test
+	public void nullPasswors() throws Exception {
+		exception.expect(IllegalArgumentException.class);
+		passwd.getSaltedHash(null);
+	}
+	
+}
