daf24b58bd4736646c7d60f2d3f80eaa34268214
Tiina Ojala <tiinamao@metropolia.fi>
2017-11-11 13:08:16 +0200

    improved responsive and added information divs to About page

--- ThePaardiHub/frontend/src/components/About.vue
+++ ThePaardiHub/frontend/src/components/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="about">
-     <p>Tämä on "about" paragraaffi, jonne Tiina askartelee paljon kivaa lässyn lässyn läätä!</p>
+     <div class="abouthost">
+      <p class="title">Host</p>
+      <p>As a host you can...</p>
+     </div>
+     <div class="aboutuser">
+      <p class="title">User</p>
+      <p>As a user you can ...</p>
+     </div>
   </div>
 </template>
 
@@ -11,10 +18,34 @@ export default{
 };
 </script>
 
-<style>
+<style scoped>
+
+  .abouthost, .aboutuser {
+    background-color: #8DB9B0;
+    width: 40%;
+    display: inline-block;
+    margin-top: 3%;
+    height: 100%;
+    padding-top: 0.5em;
+    color: white;
+  }
+
+  .title {
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+@media screen and (max-width: 790px) {
+
+  .abouthost, .aboutuser {
+    display: block;
+    margin: 3% auto 3%;
+    width: 80%;
+    height: 100%;
+  }
+}
+
 </style>
-
-
 
 
 
--- ThePaardiHub/frontend/src/components/Home.vue
+++ ThePaardiHub/frontend/src/components/Home.vue
@@ -13,7 +13,7 @@ export default {
 
 <style scoped>
 input.gameToken {
-  width: 50%;
+  width: 60%;
   height: 6em;
   margin: 0 auto;
   margin-top: 20%;
@@ -24,12 +24,80 @@ input.gameToken {
   box-shadow: 0 0 0.1em 0 grey;
 }
 button.gameToken {
-  width: 10%;
-  height: 3em;
+  width: 15%;
+  height: 100%;
   margin-top: 1em;
+  padding: 1em;
   font-size: 100%;
   background-color: #8DB9B0;
   border: none;
+}
+
+
+@media screen and (min-width: 790px) and (max-width: 1020px) {
+
+  button.gameToken {
+    width: 20%;
+    padding: 1em;
+    font-size: 1em;
+    height: 100%;
+  } 
+  
+  input.gameToken {
+  width: 75%;
+  height: 6em;
+  margin: 0 auto;
+  margin-top: 20%;
+  display:block;
+  font-size: 100%;
+  text-align: center;
+  border: none;
+  
+  }
+
+}
+
+@media screen and (max-width: 790px) {
+
+  button.gameToken {
+    width: 20%;
+    padding: 1em;
+    font-size: 1em;
+    height: 100%;
+  }
+  input.gameToken {
+  width: 75%;
+  height: 6em;
+  margin: 0 auto;
+  margin-top: 20%;
+  display:block;
+  font-size: 100%;
+  text-align: center;
+  border: none;
+  
+  }
+
+}
+@media screen and (max-width: 490px) {
+
+  button.gameToken {
+    width: 30%;
+    padding: 1em;
+    font-size: 1em;
+    height: 100%;
+  }
+
+  input.gameToken {
+  width: 90%;
+  height: 6em;
+  margin: 0 auto;
+  margin-top: 20%;
+  display:block;
+  font-size: 100%;
+  text-align: center;
+  border: none;
+  
+  }
 }
 /*button.gameToken:hover {
   background-color: #548E90;
