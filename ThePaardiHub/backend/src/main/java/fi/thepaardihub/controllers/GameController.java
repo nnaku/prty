0c745d96f016bb64710877d1e9391e1432ee0f8c
Maarit Saariniemi <maarit.saariniemi@metropolia.fi>
2017-11-13 12:58:59 +0200

    muutoksia

--- ThePaardiHub/backend/src/main/java/fi/thepaardihub/controllers/GameController.java
+++ ThePaardiHub/backend/src/main/java/fi/thepaardihub/controllers/GameController.java
@@ -8,24 +8,58 @@ import org.springframework.web.bind.annotation.RestController;
 import fi.thepaardihub.dao.games.GamesDao;
 import fi.thepaardihub.dao.users.UsersDao;
 import fi.thepaardihub.dao.games.tables.Questions;
-import fi.thepaardihub.dao.users.tables.UserAccounts;
-import fi.thepaardihub.dao.users.tables.UserRoles;
+import fi.thepaardihub.dao.games.tables.Games;
 import fi.thepaardihub.security.Password;
+import java.util.List;
 
+
+@Service
+@RestController
 public class GameController {
 
     private GamesDao games;
-    private Questions questions;
+  
 
     public GameController(GamesDao games) {
         this.games = games;
     }
 
-    public Questions getQuestions() {
-        return questions;
+    @RequestMapping("/games")
+    public @ResponseBody
+    String getGames() {
+        return "{\"message\":\"this returns user info\"}";}
+
+    public Games createGame(String author, String gameName, boolean isPrivate, String questions) {
+        try {
+            Games add = new Games();
+            add.setAuthor(author);
+            add.setGameName(gameName);
+          //  add.isPrivate(isPrivate); //En tiedä miten tää pitäs olla täälä
+            add.setQuestions(questions);
+            return add;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public void setQuestions(Questions questions) {
-        this.questions = questions;
+    public List<Games> getallGames() {
+        return games.getAllGames();
+    }
+
+    public List<Questions> getallQuestions() {
+        return games.getAllQuestions();
+    }
+    public Games getGame() {
+        try {
+			Games game = (Games) games.getAllGames();
+
+			if (game != null) {
+				return game;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
     }
 }
