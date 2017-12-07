b86aa4744419d705271e52773968b37a6a1fb53a
Maarit Saariniemi <maarit.saariniemi@metropolia.fi>
2017-12-07 11:23:05 +0200

    Game and Question querys by author and id

--- ThePaardiHub/backend/src/main/java/fi/thepaardihub/dao/games/repository/GamesRepository.java
+++ ThePaardiHub/backend/src/main/java/fi/thepaardihub/dao/games/repository/GamesRepository.java
@@ -3,7 +3,15 @@ package fi.thepaardihub.dao.games.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import fi.thepaardihub.dao.games.tables.Games;
+import fi.thepaardihub.rest.jsonobject.GameSet;
+import java.util.ArrayList;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GamesRepository extends CrudRepository<Games,Integer>{
+   @Query("from Games q where q.author=:email")
+   public ArrayList<GameSet> findByAuthor(String email);
+   
+   @Query("from Games q where q.id=:id")
+   public GameSet findByID(String id);
 
 }
--- ThePaardiHub/backend/src/main/java/fi/thepaardihub/dao/games/repository/QuestionsRepository.java
+++ ThePaardiHub/backend/src/main/java/fi/thepaardihub/dao/games/repository/QuestionsRepository.java
@@ -3,7 +3,14 @@ package fi.thepaardihub.dao.games.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import fi.thepaardihub.dao.games.tables.Questions;
+import fi.thepaardihub.rest.jsonobject.Question;
+import java.util.ArrayList;
+import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionsRepository extends CrudRepository<Questions, Integer>  {
-
+   @Query("from Questions q where q.author=:email")
+   public ArrayList<Question> findByAuthor(String email);
+   
+   @Query("from Questions q where q.id=:id")
+   public Question findByID(String id);
 }
