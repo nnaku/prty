fca9940434de050c14f63e52dc1389419c9aa957
MattiJH <mara.holopainen@gmail.com>
2017-12-09 16:11:10 +0200

    Required token for createlobby rest and made system more errorproof

--- ThePaardiHub/backend/src/main/java/fi/thepaardihub/services/LobbyService.java
+++ ThePaardiHub/backend/src/main/java/fi/thepaardihub/services/LobbyService.java
@@ -15,50 +15,62 @@ import fi.thepaardihub.socket.LobbySocket;
 
 @Service
 public class LobbyService {
-	
+
 	private GameService games;
-	private HashMap<String,Lobby> lobbies;
-	
+	private HashMap<String, Lobby> lobbies;
+
 	private LobbySocket socket;
-	
+
 	@Autowired
-	public LobbyService(GameService games,LobbySocket socket) {
+	public LobbyService(GameService games, LobbySocket socket) {
 		this.games = games;
 		this.socket = socket;
 		lobbies = new HashMap<String, Lobby>();
 	}
-	
+
 	public String[] createLobby(int GameSetId) {
-		Games game = games.getGame(GameSetId);
-		ArrayList<Questions> questions = new ArrayList<>();
-		String[] questionId = game.getQuestions().split("[;]");
-		for(String s : questionId) {
-			questions.add(games.getQuestions(Integer.parseInt(s)));
-			
+		try {
+			Games game = games.getGame(GameSetId);
+			if (game != null) {
+				ArrayList<Questions> questions = new ArrayList<>();
+				String[] questionId = game.getQuestions().split("[;]");
+				for (String s : questionId) {
+					questions.add(games.getQuestions(Integer.parseInt(s)));
+
+				}
+				String lobbieKey = "qwerty";
+
+				Lobby lobby = new Lobby(game, questions, lobbieKey);
+				socket.setLobby(lobby);
+				lobbies.put(lobbieKey, lobby);
+
+				Thread lobbyThread = new Thread(lobby);
+				lobbyThread.start();
+				String[] retVal = { "/lobby/host/show", "prty/game/host" };
+				return retVal;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-		String lobbieKey = "qwerty";
-		
-		Lobby lobby = new Lobby(game,questions,lobbieKey);
-		socket.setLobby(lobby);
-		lobbies.put(lobbieKey, lobby);
-		
-		Thread lobbyThread = new Thread(lobby);
-		lobbyThread.start();
-		String[] retVal = {"/lobby/host/show","prty/game/host" };
-		return retVal;
+
 	}
-	
-	
+
 	public String[] addPlayer(String name, String lobbyKey) {
-		
-		String playerID;
-		do {
-			playerID = UUID.randomUUID().toString();
-		}while(lobbies.get(lobbyKey).containsPlayerId(playerID));
-		lobbies.get(lobbyKey).addPlayer(playerID, new Player(name));
- 		String[] retVal = {playerID, "/lobby/play", "prty/game"};
-		return retVal;
-		
+		if (lobbies.containsKey(lobbyKey)) {
+			String playerID;
+			do {
+				playerID = UUID.randomUUID().toString();
+			} while (lobbies.get(lobbyKey).containsPlayerId(playerID));
+			lobbies.get(lobbyKey).addPlayer(playerID, new Player(name));
+			String[] retVal = { playerID, "/lobby/play", "prty/game" };
+			return retVal;
+		} else {
+			return null;
+		}
+
 	}
 
 }
