41c493b0c7e9d915a8b6aa3bb6a04aa3447862e7
Aku Kangas <aku.kangas@metropolia.fi>
2017-12-10 14:12:01 +0200

    test vue

--- ThePaardiHub/frontend/src/components/Test.vue
+++ ThePaardiHub/frontend/src/components/Test.vue
@@ -5,63 +5,111 @@
     <div>
       <button @click="tickleConnection">{{ connected ? 'disconnect' : 'connect' }}</button>
     </div>
+
+    <h2>Players</h2>
+    <button @click="addPlayer">Add player</button>
+    <p v-for="(player, index) in players" :key="index">{{ player }}</p>
+
     <h2>Send Message</h2>
-    <div>
-      <input v-model="send_message" placeholder="Send Message">
-      <button @click="send">Send</button>
-    </div>
+    <button @click="send">Get data</button>
     <h2>Message Received</h2>
-    <p v-for="(message, index) in received_messages" :key="index">{{ message.body }}</p>
+    <p v-for="(message, index) in received_messages" :key="index">{{ message }}</p>
+
+    <h2>Errors</h2>
+    <p v-for="(error, index) in errors" :key="index">{{ error }}</p>
   </div>
 </template>
 
 <script>
-import SockJS from 'sockjs-client'
-import Stomp from 'webstomp-client'
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+import axios from "axios";
+
 export default {
-  name: 'test',
-  data () {
+  name: "test",
+  data() {
     return {
+      playercount: 0,
+      players: [],
       received_messages: [],
-      received_body : "",
+      received_body: "",
+      getData: {
+        startGame: false,
+        getData: true,
+        terminateLobby: false
+      },
       send_message: null,
-      connected: false
-    }
+      connected: false,
+      erros: []
+    };
   },
   methods: {
-    send () {
-      console.log('Send message:' + this.send_message)
+    addPlayer() {
+      this.playercount++;
+      this.$nextTick(function() {
+        axios
+          .post("/addplayer", {
+            name: "player" + this.playercount,
+            key: "qwerty"
+          })
+          .then(response => {
+            this.errors = [];
+          })
+          .catch(e => {
+            this.errors.push(e);
+          });
+      });
+    },
+    send() {
       if (this.stompClient && this.stompClient.connected) {
-        this.stompClient.send('/prty/testIn', this.send_message, {})
+        this.stompClient.send(
+          "/prty/game/host",
+          JSON.stringify(this.getData),
+          {}
+        );
       }
     },
-    connect () {
-      this.socket = new SockJS('http://localhost:8080/gs-guide-websocket')
-      this.stompClient = Stomp.over(this.socket)
-      this.stompClient.connect({}, (frame) => {
-        this.connected = true
-        console.log("tämä on frame "+ frame)
-        this.stompClient.subscribe('/lobby/testOut', (tick) => {
-          console.log("tämä on tick "+ tick)
-          this.received_messages.push(JSON.parse(JSON.stringify(tick)))
-        })
-      }, (error) => {
-        console.log(error)
-        this.connected = false
-      })
+    connect() {
+      this.socket = new SockJS("/gs-guide-websocket");
+      this.stompClient = Stomp.over(this.socket);
+      this.stompClient.connect(
+        {},
+        frame => {
+          this.connected = true;
+          this.stompClient.subscribe("/lobby/host/show", tick => {
+            console.log("tämä on tick");
+            console.log(tick);
+            this.received_messages.push(tick);
+          });
+        },
+        error => {
+          console.log(error);
+          this.connected = false;
+        }
+      );
     },
-    disconnect () {
+    disconnect() {
       if (this.stompClient) {
-        this.stompClient.disconnect()
+        this.stompClient.disconnect();
       }
-      this.connected = false
+      this.connected = false;
     },
-    tickleConnection () {
-      this.connected ? this.disconnect() : this.connect()
+    tickleConnection() {
+      this.connected ? this.disconnect() : this.connect();
     }
   },
-  mounted () {
-    this.connect()
+  mounted() {
+    this.$nextTick(function() {
+      axios
+        .post("/createlobby", { id: "1" })
+        .then(response => {
+          this.errors = [];
+        })
+        .catch(e => {
+          this.errors.push(e);
+        });
+    });
+    this.connect();
   }
-}
+};
 </script>
